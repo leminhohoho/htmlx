46faa2dc59331d170ced8ffe4a7cde63be5647ad
leminhohoho <leminhnguyenai@gmail.com>
2025-07-31 01:24:24 +0700

    Add custom type for parsing string to time.Time

--- text_unmarshaller.go
+++ text_unmarshaller.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"regexp"
 	"strconv"
+	"strings"
+	"time"
 )
 
 // FloatUnitValue is a wrapper for float64 which implement [encoding.TextUnmarshaler] to convert number with unit to float64.
@@ -15,6 +17,7 @@ func (n *FloatUnitValue) UnmarshalText(text []byte) error {
 	if numStr == "" {
 		return fmt.Errorf("Unable to extract number from '%s'", string(text))
 	}
+	numStr = strings.ReplaceAll(numStr, ",", "")
 
 	num, err := strconv.ParseFloat(numStr, 64)
 	if err != nil {
@@ -37,6 +40,30 @@ func (n *IntUnitValue) UnmarshalText(text []byte) error {
 	}
 
 	*n = IntUnitValue(floatUnitValue)
+
+	return nil
+}
+
+// Time is a wrapper for time.Time which implement [encoding.TextUnmarshaler] to convert string to date.
+// Time use [Time.Layout] to determine the date layout which is used by [time.Parse], if not given it is default to 2006-01-02T15:04:05Z07:00.
+// After converting, the value can be extracted from [Time.Time]
+type Time struct {
+	Layout string
+	Time   time.Time
+}
+
+// UnmarshalText implement the [encoding.TextUnmarshaler] interface
+func (t *Time) UnmarshalText(text []byte) error {
+	var err error
+
+	if t.Layout == "" {
+		t.Layout = "2006-01-02T15:04:05Z07:00"
+	}
+
+	t.Time, err = time.Parse(t.Layout, string(text))
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
